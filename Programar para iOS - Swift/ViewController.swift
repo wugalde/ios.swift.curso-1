8196377cb561fea8aa2d924010ab7ea2f5a698b8
Walter Ugalde <walter.ugalde@gmail.com>
2016-04-17 17:42:45 -0600

    Testing

---
+++ Programar para iOS - Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Programar para iOS - Swift
+//
+//  Created by Developer on 4/17/16.
+//  Copyright © 2016 Developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
